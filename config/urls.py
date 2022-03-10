db9e6dd776f5da13bea9b4cbd69dab2cc95b5ff9
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-03-10 20:20:30 +0330

    add media urls to config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,8 @@ from django.contrib import admin
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
 	openapi.Info(
@@ -24,3 +26,5 @@ urlpatterns = [
 	re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
 	re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
