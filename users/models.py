62720c89b47ddc1fedad8748309c358f9ae385a5
abdolrahman kiany <94900618+abdoki7788@users.noreply.github.com>
2022-03-10 13:37:28 +0330

    add followers field to user model

--- users/models.py
+++ users/models.py
@@ -6,5 +6,6 @@ from .managers import UserManager
 # Create your models here.
 
 class CustomUser(AbstractUser):
-	profile = models.ImageField()
+	profile = models.ImageField(default='images/logo.png')
+	followers = models.ManyToManyField("self", blank=True)
 	objects = UserManager()
